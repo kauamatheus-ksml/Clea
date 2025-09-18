ee89202a7316a48de1ee0b967e417167571f401b
kauamatheus-ksml <148459355+kauamatheus-ksml@users.noreply.github.com>
2025-09-18 10:26:51 -0300

    first commit

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+// Configuração do Banco de Dados Remoto (Hostinger)
+define('DB_HOST', 'srv406.hstgr.io'); // Ex: 123.456.789.10
+define('DB_USER', 'u383946504_cleacasamentos');       // Ex: u123456789_clea
+define('DB_PASS', 'Aaku_2004@');
+define('DB_NAME', 'u383946504_cleacasamentos');         // Ex: u123456789_clea_db
+
+// Configuração Base da Aplicação
+define('APP_NAME', 'Clea Casamentos');
+define('BASE_URL', 'https://www.cleacasamentos.com.br'); // URL do seu site
+
+// Outras configurações (chaves de API, etc.)
+define('STRIPE_API_KEY', 'sua_chave_stripe_aqui');
+define('CLICKSIGN_API_KEY', 'sua_chave_clicksign_aqui');
+
+?>
